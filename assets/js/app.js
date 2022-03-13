e9c65d46c767f5fc99f8f5e60011dacbe3a0e4b0
Tusar <tusarahmed141@gmail.com>
2022-03-13 18:16:39 +0600

    Test on Math Object in js

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+// Test Math.abs();
+// const myNumber = 0;
+// const absoluteNumber = Math.abs(myNumber)
+// console.log(absoluteNumber);
+
+// Test Math.ceil();
+// const myNumber = 4.1;
+// const output = Math.ceil(myNumber);
+// console.log(output);
+
+// Test Math.floor()
+// const myNumber = -0.9;
+// const output = Math.floor(myNumber);
+// console.log(output);
+
+// TEst Math.round()
+// const myNumber = 4.4;
+// const output = Math.round(myNumber);
+// console.log(output);
+
+// Test Random Number
+const myRandomNumber = Math.random();
+const multiplyByTen = myRandomNumber * 10;
+const toRoundMyRandomNumber = Math.round(multiplyByTen)
+console.log(toRoundMyRandomNumber);
