f4f871f891fbd3a1e659d3abefa232623bdd8f2b
Tusar <tusarahmed141@gmail.com>
2022-03-13 18:34:57 +0600

    Generate arbitary random numbers

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,7 +19,18 @@
 // console.log(output);
 
 // Test Random Number
-const myRandomNumber = Math.random();
-const multiplyByTen = myRandomNumber * 10;
-const toRoundMyRandomNumber = Math.round(multiplyByTen)
-console.log(toRoundMyRandomNumber);
+// const myRandomNumber = Math.random();
+// const multiplyByTen = myRandomNumber * 10;
+// const toRoundMyRandomNumber = Math.round(multiplyByTen)
+// console.log(toRoundMyRandomNumber);
+
+// Generate Random Number in 10 Times
+const customRanNumberGenerate = (min, max, times) => {
+  for (let i = 0; i < times; i++) {
+    const customRandomNumber = Math.round(Math.random() * (max - min) + min);
+    console.log(customRandomNumber);
+  }
+};
+customRanNumberGenerate(1, 5, 10);
+
+// Swapping Variable
